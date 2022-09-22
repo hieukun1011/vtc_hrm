b3c040e2654e174587adb5121f20c1a4ac5913fc
hunghaitu <65693367+hunghaitu@users.noreply.github.com>
2022-09-22 17:01:59 +0700

    Sửa chức năng check tên giai đoạn nhiệm vụ

--- diligo_hrm/addons_common/project_custom/models/project_task_type.py
+++ diligo_hrm/addons_common/project_custom/models/project_task_type.py
@@ -12,17 +12,24 @@ class ProjectTaskType(models.Model):
     @api.constrains('name')
     def _constrains_name_not_duplicate(self):
         list_names = self.env['project.task.type'].search([]).mapped('name')
-        list = []
-        for each in list_names:
-            if each.upper() not in list:
-                list.append(each.upper())
-            if each.lower() not in list:
-                list.append(each.lower())
-            if each not in list:
-                list.append(each)
-
         for rec in self:
-            if rec.name and rec.name in list:
+            if rec.name in list_names:
+                list_names.remove(rec.name)
+            if rec.name in list_names or rec.name.upper() in list_names or rec.name.lower() in list_names:
                 raise ValidationError("You already have that name. Please give another name.")
+        # list = []
+        # for each in list_names:
+        #     if each.upper() not in list:
+        #         list.append(each.upper())
+        #     if each.lower() not in list:
+        #         list.append(each.lower())
+        #     if each not in list:
+        #         list.append(each)
+        # print('list names ', list)
+        #
+        # for rec in self:
+        #     print(rec.name)
+        #     if rec.name and rec.name in list:
+        #         raise ValidationError("You already have that name. Please give another name.")
 
 
