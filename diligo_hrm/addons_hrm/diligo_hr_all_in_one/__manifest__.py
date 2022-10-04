d1ff63225ba1eb6ff46ef931db6b41e26843958b
Dương Trung Hiếu <hieukun1011@gmail.com>
2022-10-04 15:31:02 +0700

    update

--- diligo_hrm/addons_hrm/diligo_hr_all_in_one/__manifest__.py
+++ diligo_hrm/addons_hrm/diligo_hr_all_in_one/__manifest__.py
@@ -11,9 +11,12 @@
                 'diligo_maintenance',
                 'garazd_product_label',
                 'base',
-                # 'website',
+                'website',
                 'base_unit_vn',
                 'fims_login_background_and_styles',
+                'to_attendance_device',
+                'om_hr_payroll_account',
+                'hr_insurance',
                 ],
     'data': [
         'views/menu_item.xml'
