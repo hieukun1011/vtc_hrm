891a8f8c90884bddeba6ab90940ce99bb429163b
hunghaitu <65693367+hunghaitu@users.noreply.github.com>
2022-10-07 10:34:31 +0700

    ẩn code

--- diligo_hrm/addons_common/project_custom/models/project_task_type.py
+++ diligo_hrm/addons_common/project_custom/models/project_task_type.py
@@ -9,22 +9,22 @@ class ProjectTaskType(models.Model):
         if self.name:
             self.name = self.name.strip()
 
-    @api.constrains('name')
-    def _constrains_name_not_duplicate(self):
-        list_names = self.env['project.task.type'].search([]).mapped('name')
-        list_names.remove(self.name)
-        list = []
-        for each in list_names:
-            if each.upper() not in list:
-                list.append(each.upper())
-            if each.lower() not in list:
-                list.append(each.lower())
-            if each not in list:
-                list.append(each)
-
-        if self.name:
-            if self.name in list or self.name.upper() in list or self.name.lower() in list:
-                raise ValidationError("You already have that name. Please give another name.")
+    # @api.constrains('name')
+    # def _constrains_name_not_duplicate(self):
+    #     list_names = self.env['project.task.type'].search([]).mapped('name')
+    #     list_names.remove(self.name)
+    #     list = []
+    #     for each in list_names:
+    #         if each.upper() not in list:
+    #             list.append(each.upper())
+    #         if each.lower() not in list:
+    #             list.append(each.lower())
+    #         if each not in list:
+    #             list.append(each)
+    #
+    #     if self.name:
+    #         if self.name in list or self.name.upper() in list or self.name.lower() in list:
+    #             raise ValidationError("You already have that name. Please give another name.")
 
 
     # def unlink(self):
